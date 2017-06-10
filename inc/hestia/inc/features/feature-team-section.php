49edd662fa49128d0cce28f4b2bdd9d04e4286db
abaicus <baicusandrei@me.com>
2017-06-10 16:32:13 +0300

    Added selective refresh.

--- inc/hestia/inc/features/feature-team-section.php
+++ inc/hestia/inc/features/feature-team-section.php
@@ -2,8 +2,7 @@
 /**
  * Customizer functionality for the Team section.
  *
- * @package WordPress
- * @subpackage Hestia
+ * @package Hestia
  * @since Hestia 1.0
  */
 
@@ -18,11 +17,13 @@ if ( ! function_exists( 'hestia_team_customize_register' ) ) :
 	 * Hook controls for Team section to Customizer.
 	 *
 	 * @since Hestia 1.0
+	 * @modified 1.1.30
 	 */
 	function hestia_team_customize_register( $wp_customize ) {
 
+		$selective_refresh = isset( $wp_customize->selective_refresh ) ? true : false;
 		$wp_customize->add_section( 'hestia_team', array(
-			'title'    => esc_html__( 'Team', 'hestia-companion', 'themeisle-companion' ),
+			'title'    => esc_html__( 'Team', 'themeisle-companion' ),
 			'panel'    => 'hestia_frontpage_sections',
 			'priority' => apply_filters( 'hestia_section_priority', 30, 'hestia_team' ),
 		) );
@@ -34,29 +35,29 @@ if ( ! function_exists( 'hestia_team_customize_register' ) ) :
 
 		$wp_customize->add_control( 'hestia_team_hide', array(
 			'type'     => 'checkbox',
-			'label'    => esc_html__( 'Disable section', 'hestia-companion', 'themeisle-companion' ),
+			'label'    => esc_html__( 'Disable section', 'themeisle-companion' ),
 			'section'  => 'hestia_team',
 			'priority' => 1,
 		) );
 
 		$wp_customize->add_setting( 'hestia_team_title', array(
 			'sanitize_callback' => 'sanitize_text_field',
-			'transport'         => 'postMessage',
+			'transport'         => $selective_refresh ? 'postMessage' : 'refresh',
 		) );
 
 		$wp_customize->add_control( 'hestia_team_title', array(
-			'label'    => esc_html__( 'Section Title', 'hestia-companion', 'themeisle-companion' ),
+			'label'    => esc_html__( 'Section Title', 'themeisle-companion' ),
 			'section'  => 'hestia_team',
 			'priority' => 5,
 		) );
 
 		$wp_customize->add_setting( 'hestia_team_subtitle', array(
 			'sanitize_callback' => 'sanitize_text_field',
-			'transport'         => 'postMessage',
+			'transport'         => $selective_refresh ? 'postMessage' : 'refresh',
 		) );
 
 		$wp_customize->add_control( 'hestia_team_subtitle', array(
-			'label'    => esc_html__( 'Section Subtitle', 'hestia-companion', 'themeisle-companion' ),
+			'label'    => esc_html__( 'Section Subtitle', 'themeisle-companion' ),
 			'section'  => 'hestia_team',
 			'priority' => 10,
 		) );
@@ -64,14 +65,15 @@ if ( ! function_exists( 'hestia_team_customize_register' ) ) :
 		if ( class_exists( 'Hestia_Repeater' ) ) {
 			$wp_customize->add_setting( 'hestia_team_content', array(
 				'sanitize_callback' => 'hestia_repeater_sanitize',
+				'transport'         => $selective_refresh ? 'postMessage' : 'refresh',
 			) );
 
 			$wp_customize->add_control( new Hestia_Repeater( $wp_customize, 'hestia_team_content', array(
-				'label'                                => esc_html__( 'Team Content', 'hestia-companion', 'themeisle-companion' ),
+				'label'                                => esc_html__( 'Team Content', 'themeisle-companion' ),
 				'section'                              => 'hestia_team',
 				'priority'                             => 15,
-				'add_field_label'                      => esc_html__( 'Add new Team Member', 'hestia-companion', 'themeisle-companion' ),
-				'item_name'                            => esc_html__( 'Team Member', 'hestia-companion', 'themeisle-companion' ),
+				'add_field_label'                      => esc_html__( 'Add new Team Member', 'themeisle-companion' ),
+				'item_name'                            => esc_html__( 'Team Member', 'themeisle-companion' ),
 				'customizer_repeater_image_control'    => true,
 				'customizer_repeater_title_control'    => true,
 				'customizer_repeater_subtitle_control' => true,
@@ -85,3 +87,71 @@ if ( ! function_exists( 'hestia_team_customize_register' ) ) :
 	add_action( 'customize_register', 'hestia_team_customize_register' );
 
 endif;
+
+
+/**
+ * Add selective refresh for team section controls.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ * @since 1.1.30
+ * @access public
+ */
+function hestia_register_team_partials( $wp_customize ) {
+	// Abort if selective refresh is not available.
+	if ( ! isset( $wp_customize->selective_refresh ) ) {
+		return;
+	}
+
+	$wp_customize->selective_refresh->add_partial( 'hestia_team_title', array(
+		'selector' => '#team h2.title',
+		'settings' => 'hestia_team_title',
+		'render_callback' => 'hestia_team_title_callback',
+	));
+
+	$wp_customize->selective_refresh->add_partial( 'hestia_team_subtitle', array(
+		'selector' => '#team h5.description',
+		'settings' => 'hestia_team_subtitle',
+		'render_callback' => 'hestia_team_subtitle_callback',
+	));
+
+	$wp_customize->selective_refresh->add_partial( 'hestia_team_content', array(
+		'selector' => '.hestia-team-content',
+		'settings' => 'hestia_team_content',
+		'render_callback' => 'hestia_team_content_callback',
+	));
+}
+add_action( 'customize_register', 'hestia_register_team_partials' );
+
+
+/**
+ * Callback function for team title selective refresh.
+ *
+ * @return string
+ * @since 1.1.30
+ * @access public
+ */
+function hestia_team_title_callback() {
+	return get_theme_mod( 'hestia_team_title' );
+}
+
+/**
+ * Callback function for team subtitle selective refresh.
+ *
+ * @return string
+ * @since 1.1.30
+ * @access public
+ */
+function hestia_team_subtitle_callback() {
+	return get_theme_mod( 'hestia_team_subtitle' );
+}
+
+/**
+ * Callback function for team content selective refresh.
+ *
+ * @since 1.1.30
+ * @access public
+ */
+function hestia_team_content_callback() {
+	$hestia_team_content = get_theme_mod( 'hestia_team_content' );
+	hestia_team_content( $hestia_team_content, true );
+}
--- inc/hestia/inc/sections/hestia-testimonials-section.php
+++ inc/hestia/inc/sections/hestia-testimonials-section.php
@@ -2,8 +2,7 @@
 /**
  * Testimonials section for the homepage.
  *
- * @package WordPress
- * @subpackage Hestia
+ * @package Hestia
  * @since Hestia 1.0
  */
 
@@ -15,37 +14,17 @@ if ( ! function_exists( 'hestia_testimonials' ) ) :
 	 */
 	function hestia_testimonials() {
 
-		if ( current_user_can('edit_theme_options' ) ) {
-			$hestia_testimonials_title    = get_theme_mod( 'hestia_testimonials_title', esc_html__( 'What clients say', 'themeisle-companion' ) );
-			$hestia_testimonials_subtitle = get_theme_mod( 'hestia_testimonials_subtitle', esc_html__( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'themeisle-companion' ) );
-			$hestia_testimonials_content  = get_theme_mod( 'hestia_testimonials_content', json_encode( array(
-				array(
-					'image_url' => get_template_directory_uri() . '/assets/img/5.jpg',
-					'title'     => esc_html__( 'Inverness McKenzie', 'themeisle-companion' ),
-					'subtitle'  => esc_html__( 'Business Owner', 'themeisle-companion' ),
-					'text'      => esc_html__( '"We have no regrets! After using your product my business skyrocketed! I made back the purchase price in just 48 hours! I couldn\'t have asked for more than this."', 'themeisle-companion' ),
-					'id'        => 'customizer_repeater_56d7ea7f40d56',
-				),
-				array(
-					'image_url' => get_template_directory_uri() . '/assets/img/6.jpg',
-					'title'     => esc_html__( 'Hanson Deck', 'themeisle-companion' ),
-					'subtitle'  => esc_html__( 'Independent Artist', 'themeisle-companion' ),
-					'text'      => esc_html__( '"Your company is truly upstanding and is behind its product 100 percent. Hestia is worth much more than I paid. I like Hestia more each day because it makes easier."', 'themeisle-companion' ),
-					'id'        => 'customizer_repeater_56d7ea7f40d66',
-				),
-				array(
-					'image_url' => get_template_directory_uri() . '/assets/img/7.jpg',
-					'title'     => esc_html__( 'Natalya Undergrowth', 'themeisle-companion' ),
-					'subtitle'  => esc_html__( 'Freelancer', 'themeisle-companion' ),
-					'text'      => esc_html__( '"Thank you for making it painless, pleasant and most of all hassle free! I am so pleased with this product. Dude, your stuff is great! I will refer everyone I know."', 'themeisle-companion' ),
-					'id'        => 'customizer_repeater_56d7ea7f40d76',
-				),
-			) ) );
-		} else {
-			$hestia_testimonials_title    = get_theme_mod( 'hestia_testimonials_title' );
-			$hestia_testimonials_subtitle = get_theme_mod( 'hestia_testimonials_subtitle' );
-			$hestia_testimonials_content  = get_theme_mod( 'hestia_testimonials_content' );
+		$default_title = '';
+		$default_subtitle = '';
+		$default_content = '';
+		if ( current_user_can( 'edit_theme_options' ) ) {
+			$default_title    = __( 'What clients say', 'themeisle-companion' );
+			$default_subtitle = __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'themeisle-companion' );
+			$default_content  = hestia_get_testimonials_default();
 		}
+		$hestia_testimonials_title    = get_theme_mod( 'hestia_testimonials_title', $default_title );
+		$hestia_testimonials_subtitle = get_theme_mod( 'hestia_testimonials_subtitle', $default_subtitle );
+		$hestia_testimonials_content  = get_theme_mod( 'hestia_testimonials_content', $default_content );
 
 		$hide_section = get_theme_mod( 'hestia_testimonials_hide', false );
 		if ( ( (bool) $hide_section === true ) || ( empty( $hestia_testimonials_title ) && empty( $hestia_testimonials_subtitle ) && empty( $hestia_testimonials_content ) ) ) {
@@ -65,84 +44,119 @@ if ( ! function_exists( 'hestia_testimonials' ) ) :
 						<?php endif; ?>
 					</div>
 				</div>
-				<div class="row">
-					<?php
-					if ( ! empty( $hestia_testimonials_content ) ) :
-						$hestia_testimonials_content = json_decode( $hestia_testimonials_content );
-						foreach ( $hestia_testimonials_content as $testimonial_item ) :
-							$image = ! empty( $testimonial_item->image_url ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->image_url, 'Testimonials section' ) : '';
-							$title = ! empty( $testimonial_item->title ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->title, 'Testimonials section' ) : '';
-							$subtitle = ! empty( $testimonial_item->subtitle ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->subtitle, 'Testimonials section' ) : '';
-							$text = ! empty( $testimonial_item->text ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->text, 'Testimonials section' ) : '';
-							$link = ! empty( $testimonial_item->link ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->link, 'Testimonials section' ) : '';
-							?>
-							<div class="col-md-4">
-								<div class="card card-testimonial card-plain">
-									<?php if ( ! empty( $image ) ) : ?>
-										<div class="card-avatar">
-											<?php if ( ! empty( $link ) ) : ?>
-											<a href="<?php echo esc_url( $link ); ?>">
-												<?php endif; ?>
-												<img class="img"
-													 src="<?php echo esc_url( $image ); ?>" <?php if ( ! empty( $title ) ) : ?> alt="<?php echo esc_attr( $title ); ?>" title="<?php echo esc_attr( $title ); ?>" <?php endif; ?> />
-												<?php if ( ! empty( $link ) ) : ?>
-											</a>
-										<?php endif; ?>
-										</div>
-									<?php endif; ?>
-									<div class="content">
-										<?php if ( ! empty( $title ) ) : ?>
-											<h4 class="card-title"><?php echo esc_html( $title ); ?></h4>
-										<?php endif; ?>
-										<?php if ( ! empty( $subtitle ) ) : ?>
-											<h6 class="category text-muted"><?php echo esc_html( $subtitle ); ?></h6>
-										<?php endif; ?>
-										<?php if ( ! empty( $text ) ) : ?>
-											<p class="card-description"><?php echo esc_html( $text ); ?></p>
-										<?php endif; ?>
-									</div>
-								</div>
-							</div>
-							<?php
-						endforeach;
-					endif;
-					?>
-				</div>
+				<?php hestia_testimonials_content( $hestia_testimonials_content ); ?>
 			</div>
 		</section>
 		<?php
 	}
 endif;
 
+
+/**
+ * Display content for testimonials section.
+ *
+ * @since 1.1.30
+ * @access public
+ * @param string $hestia_testimonials_content Section content in json format.
+ * @param bool   $is_callback Flag to check if it's callback or not.
+ */
+function hestia_testimonials_content( $hestia_testimonials_content, $is_callback = false ) {
+
+	if ( ! $is_callback ) { ?>
+		<div class="row hestia-testimonials-content">
+		<?php
+	}
+	if ( ! empty( $hestia_testimonials_content ) ) :
+		$allowed_html = array(
+			'br'     => array(),
+			'em'     => array(),
+			'strong' => array(),
+			'b'      => array(),
+			'i'      => array(),
+		);
+		$hestia_testimonials_content = json_decode( $hestia_testimonials_content );
+		foreach ( $hestia_testimonials_content as $testimonial_item ) :
+			$image = ! empty( $testimonial_item->image_url ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->image_url, 'Testimonials section' ) : '';
+			$title = ! empty( $testimonial_item->title ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->title, 'Testimonials section' ) : '';
+			$subtitle = ! empty( $testimonial_item->subtitle ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->subtitle, 'Testimonials section' ) : '';
+			$text = ! empty( $testimonial_item->text ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->text, 'Testimonials section' ) : '';
+			$link = ! empty( $testimonial_item->link ) ? apply_filters( 'hestia_translate_single_string', $testimonial_item->link, 'Testimonials section' ) : '';
+			?>
+			<div class="col-md-4">
+				<div class="card card-testimonial card-plain">
+					<?php if ( ! empty( $image ) ) : ?>
+						<div class="card-avatar">
+							<?php if ( ! empty( $link ) ) : ?>
+							<a href="<?php echo esc_url( $link ); ?>">
+								<?php endif; ?>
+								<img class="img"
+									 src="<?php echo esc_url( $image ); ?>" <?php if ( ! empty( $title ) ) : ?> alt="<?php echo esc_attr( $title ); ?>" title="<?php echo esc_attr( $title ); ?>" <?php endif; ?> />
+								<?php if ( ! empty( $link ) ) : ?>
+							</a>
+						<?php endif; ?>
+						</div>
+					<?php endif; ?>
+					<div class="content">
+						<?php if ( ! empty( $title ) ) : ?>
+							<h4 class="card-title"><?php echo esc_html( $title ); ?></h4>
+						<?php endif; ?>
+						<?php if ( ! empty( $subtitle ) ) : ?>
+							<h6 class="category text-muted"><?php echo esc_html( $subtitle ); ?></h6>
+						<?php endif; ?>
+						<?php if ( ! empty( $text ) ) : ?>
+							<p class="card-description"><?php echo wp_kses( html_entity_decode( $text ), $allowed_html ); ?></p>
+						<?php endif; ?>
+					</div>
+				</div>
+			</div>
+			<?php
+		endforeach;
+	endif;
+	if ( ! $is_callback ) { ?>
+		</div>
+		<?php
+	}
+}
+
+
+/**
+ * Get default values for testimonials section.
+ *
+ * @since 1.1.30
+ * @access public
+ */
+function hestia_get_testimonials_default() {
+	return apply_filters( 'hestia_testimonials_default_content', json_encode( array(
+		array(
+			'image_url' => get_template_directory_uri() . '/assets/img/5.jpg',
+			'title'     => esc_html__( 'Inverness McKenzie', 'themeisle-companion' ),
+			'subtitle'  => esc_html__( 'Business Owner', 'themeisle-companion' ),
+			'text'      => esc_html__( '"We have no regrets! After using your product my business skyrocketed! I made back the purchase price in just 48 hours! I couldn\'t have asked for more than this."', 'themeisle-companion' ),
+			'id'        => 'customizer_repeater_56d7ea7f40d56',
+		),
+		array(
+			'image_url' => get_template_directory_uri() . '/assets/img/6.jpg',
+			'title'     => esc_html__( 'Hanson Deck', 'themeisle-companion' ),
+			'subtitle'  => esc_html__( 'Independent Artist', 'themeisle-companion' ),
+			'text'      => esc_html__( '"Your company is truly upstanding and is behind its product 100 percent. Hestia is worth much more than I paid. I like Hestia more each day because it makes easier."', 'themeisle-companion' ),
+			'id'        => 'customizer_repeater_56d7ea7f40d66',
+		),
+		array(
+			'image_url' => get_template_directory_uri() . '/assets/img/7.jpg',
+			'title'     => esc_html__( 'Natalya Undergrowth', 'themeisle-companion' ),
+			'subtitle'  => esc_html__( 'Freelancer', 'themeisle-companion' ),
+			'text'      => esc_html__( '"Thank you for making it painless, pleasant and most of all hassle free! I am so pleased with this product. Dude, your stuff is great! I will refer everyone I know."', 'themeisle-companion' ),
+			'id'        => 'customizer_repeater_56d7ea7f40d76',
+		),
+	) ) );
+}
+
 if ( ! function_exists( 'hestia_testimonials_register_strings' ) ) {
 	/**
 	 * Register polylang strings
 	 */
 	function hestia_testimonials_register_strings() {
-		$default = json_encode( array(
-			array(
-				'image_url' => get_template_directory_uri() . '/assets/img/5.jpg',
-				'title'     => esc_html__( 'Inverness McKenzie', 'themeisle-companion' ),
-				'subtitle'  => esc_html__( 'Business Owner', 'themeisle-companion' ),
-				'text'      => esc_html__( '"We have no regrets! After using your product my business skyrocketed! I made back the purchase price in just 48 hours! I couldn\'t have asked for more than this."', 'themeisle-companion' ),
-				'id'        => 'customizer_repeater_56d7ea7f40d56',
-			),
-			array(
-				'image_url' => get_template_directory_uri() . '/assets/img/6.jpg',
-				'title'     => esc_html__( 'Hanson Deck', 'themeisle-companion' ),
-				'subtitle'  => esc_html__( 'Independent Artist', 'themeisle-companion' ),
-				'text'      => esc_html__( '"Your company is truly upstanding and is behind its product 100 percent. Hestia is worth much more than I paid. I like Hestia more each day because it makes easier."', 'themeisle-companion' ),
-				'id'        => 'customizer_repeater_56d7ea7f40d66',
-			),
-			array(
-				'image_url' => get_template_directory_uri() . '/assets/img/7.jpg',
-				'title'     => esc_html__( 'Natalya Undergrowth', 'themeisle-companion' ),
-				'subtitle'  => esc_html__( 'Freelancer', 'themeisle-companion' ),
-				'text'      => esc_html__( '"Thank you for making it painless, pleasant and most of all hassle free! I am so pleased with this product. Dude, your stuff is great! I will refer everyone I know."', 'themeisle-companion' ),
-				'id'        => 'customizer_repeater_56d7ea7f40d76',
-			),
-		) );
-
+		$default = hestia_get_testimonials_default();
 		hestia_pll_string_register_helper( 'hestia_testimonials_content', $default, 'Testimonials section' );
 	}
 }
