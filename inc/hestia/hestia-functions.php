c669a90d76b9c28d8164f9d3710f22243d909abf
cristian-ungureanu <cristian@themeisle.com>
2017-06-13 16:47:22 +0300

    #30 - create frontpage and set it automatically

--- inc/hestia/hestia-functions.php
+++ inc/hestia/hestia-functions.php
@@ -58,3 +58,70 @@ function themeisle_hestia_require() {
 }
 
 add_action( 'after_setup_theme', 'themeisle_hestia_require' );
+
+
+/**
+ *
+ */
+function themeisle_hestia_set_frontpage(){
+
+    if ( function_exists( 'hestia_setup_theme' ) ) {
+        $is_fresh_site = get_option('fresh_site');
+        if ((bool)$is_fresh_site === false) {
+
+            $frontpage_title = esc_html__('Front Page', 'themeisle-companion');
+            $front_id = themeisle_hestia_create_page('hestia-front', $frontpage_title);
+
+            $blogpage_title = esc_html__('Blog', 'themeisle-companion');
+            $blog_id = themeisle_hestia_create_page('blog', $blogpage_title);
+
+            set_theme_mod('show_on_front','page');
+
+            update_option('show_on_front', 'page');
+            if (!empty($front_id)) {
+                update_option('page_on_front', $front_id);
+            }
+
+            if (!empty($blog_id)) {
+                update_option('page_for_posts', $blog_id);
+            }
+        }
+    }
+}
+add_action( 'after_switch_theme', 'themeisle_hestia_set_frontpage' );
+
+/**
+ * Function that checks if a page with a slug exists. If not, it create one.
+ * @param string $slug Page slug.
+ * @param string $page_title Page title.
+ * @return int
+ */
+function themeisle_hestia_create_page( $slug, $page_title ){
+
+    //Check if page exists
+    $args = array(
+        'name'        => $slug,
+        'post_type'   => 'page',
+        'post_status' => 'publish',
+        'numberposts' => 1
+    );
+    $post = get_posts($args);
+
+
+    if( !empty( $post ) ) {
+        $page_id = $post[0]->ID;
+    } else {
+        // Page doesn't exist. Create one.
+        $postargs = array(
+            'post_type'     => 'page',
+            'post_name'     => $slug,
+            'post_title'    => $page_title,
+            'post_status'   => 'publish',
+            'post_author'   => 1,
+        );
+
+        // Insert the post into the database
+        $page_id = wp_insert_post( $postargs );
+    }
+    return $page_id;
+}
