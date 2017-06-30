6252ba3a41c4692de9c6ccd444a125365ca34cfb
Bogdan Preda <bogdan.preda@themeisle.com>
2017-06-30 16:58:24 +0300

    WIP: PHPCS

--- obfx_modules/test/init.php
+++ obfx_modules/test/init.php
@@ -1,18 +1,85 @@
 <?php
+/**
+ * The Mock-up to demonstrate and test module use.
+ *
+ * @link       https://themeisle.com
+ * @since      1.0.0
+ *
+ * @package    Test_OBFX_Module
+ */
+
+/**
+ * The class defines a new module to be used by Orbit Fox plugin.
+ *
+ * @package    Test_OBFX_Module
+ * @author     Themeisle <friends@themeisle.com>
+ */
 class Test_OBFX_Module extends Orbit_Fox_Module_Abstract {
 
+	/**
+	 * Test_OBFX_Module constructor.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public function __construct() {
+		$this->name = __( 'Orbit Fox Test Module', 'obfx' );
+		$this->description = __( 'A test module for Orbit Fox.', 'obfx' );
+	}
 
+	/**
+	 * Determine if module should be loaded.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return bool
+	 */
 	public function enable_module() {
 		return true;
 	}
 
+	/**
+	 * The loading logic for the module.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
 	public function load() {
 
 	}
 
-	public function public_enqueue(){}
+	/**
+	 * Method that returns an array of scripts and styles to be loaded
+	 * for the front end part.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function public_enqueue() {
+	    return array();
+	}
 
-	public function admin_enqueue(){}
+	/**
+	 * Method that returns an array of scripts and styles to be loaded
+	 * for the admin part.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function admin_enqueue() {
+		return array();
+	}
 
-	public function options(){}
+	/**
+	 * Method to define the options fields for the module
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function options() {
+		return array();
+	}
 }
