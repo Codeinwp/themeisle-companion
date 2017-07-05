72087eb9688871b28e0ba73a42bfabee4d3ce8e2
Bogdan Preda <bogdan.preda@themeisle.com>
2017-07-05 16:26:13 +0300

    WIP: Fixes

--- orbit-fox.php
+++ orbit-fox.php
@@ -34,8 +34,8 @@ if ( ! defined( 'WPINC' ) ) {
  * This action is documented in core/includes/class-orbit-fox-activator.php
  */
 function activate_orbit_fox() {
-	$orbit_fox_activator = new Orbit_Fox_Activator();
-	$orbit_fox_activator::activate();
+	$obfx_activator = new Orbit_Fox_Activator();
+    $obfx_activator::activate();
 }
 
 /**
@@ -43,8 +43,8 @@ function activate_orbit_fox() {
  * This action is documented in core/includes/class-orbit-fox-deactivator.php
  */
 function deactivate_orbit_fox() {
-	$orbit_fox_deactivator = new Orbit_Fox_Deactivator();
-	$orbit_fox_deactivator::deactivate();
+	$obfx_deactivator = new Orbit_Fox_Deactivator();
+    $obfx_deactivator::deactivate();
 }
 
 register_activation_hook( __FILE__, 'activate_orbit_fox' );
