56ad92f544c976ccc6e079470b39ad105b7aa23f
selu91 <cristea.marius.alexandru@gmail.com>
2017-08-11 15:59:38 +0300

    Fixed duplicate id usage.

--- core/app/views/modules-page.php
+++ core/app/views/modules-page.php
@@ -34,41 +34,42 @@ if ( ! isset( $panels ) ) {
 	$panels = '';
 }
 ?>
-<div id="obfx-wrapper" class="obfx-header">
-    <div class="obfx-header-content">
-        <img src="<?php echo OBFX_URL; ?>/images/orbit-fox.png" title="Orbit Fox" class="obfx-logo"/>
-        <h1><?php echo __( 'Orbit Fox Companion', 'themeisle-companion' ); ?></h1><span class="powered"> by <a href="https://themeisle.com" target="_blank"><b>ThemeIsle</b></a></span>
-    </div>
+<div class="obfx-wrapper obfx-header">
+	<div class="obfx-header-content">
+		<img src="<?php echo OBFX_URL; ?>/images/orbit-fox.png" title="Orbit Fox" class="obfx-logo"/>
+		<h1><?php echo __( 'Orbit Fox Companion', 'themeisle-companion' ); ?></h1><span class="powered"> by <a
+					href="https://themeisle.com" target="_blank"><b>ThemeIsle</b></a></span>
+	</div>
 </div>
 <div id="obfx-wrapper" style="padding: 0; margin-top: 10px; margin-bottom: 5px;">
 	<?php
-		echo $toasts;
+	echo $toasts;
 	?>
 </div>
-<div id="obfx-wrapper">
+<div class="obfx-wrapper" id="obfx-modules-wrapper">
 	<?php
 	if ( $no_modules ) {
 		echo $empty_tpl;
 	} else {
-	?>
-	<div class="panel">
-		<div class="panel-header text-center">
-			<div class="panel-title mt-10"><?php echo __( 'Available Modules', 'themeisle-companion' ); ?></div>
+		?>
+		<div class="panel">
+			<div class="panel-header text-center">
+				<div class="panel-title mt-10"><?php echo __( 'Available Modules', 'themeisle-companion' ); ?></div>
+			</div>
+			<div class="panel-body">
+				<?php echo $tiles; ?>
+			</div>
+			<div class="panel-footer">
+				<!-- buttons or inputs -->
+			</div>
 		</div>
-		<div class="panel-body">
-			<?php echo $tiles; ?>
+		<div class="panel">
+			<div class="panel-header text-center">
+				<div class="panel-title mt-10"><?php echo __( 'Activated Modules Options', 'themeisle-companion' ); ?></div>
+			</div>
+			<?php echo ( $panels == '' ) ? '<p class="text-center">' . __( 'No modules activated.', 'themeisle-companion' ) . '</p>' : $panels; ?>
 		</div>
-		<div class="panel-footer">
-			<!-- buttons or inputs -->
-		</div>
-	</div>
-	<div class="panel">
-		<div class="panel-header text-center">
-			<div class="panel-title mt-10"><?php echo __( 'Activated Modules Options', 'themeisle-companion' ); ?></div>
-		</div>
-	<?php echo ( $panels == '' ) ? '<p class="text-center">' . __( 'No modules activated.', 'themeisle-companion' ) . '</p>' : $panels; ?>
-	</div>
-	<?php
+		<?php
 	}
 	?>
 </div>
