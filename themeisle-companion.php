75e4330d4dcbdaeedb28358bbe609b0be5a64bdb
selu91 <cristea.marius.alexandru@gmail.com>
2017-08-11 11:48:45 +0300

    Fixed languages dir.

--- themeisle-companion.php
+++ themeisle-companion.php
@@ -21,7 +21,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       themeisle-companion
- * Domain Path:       /core/languages
+ * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
