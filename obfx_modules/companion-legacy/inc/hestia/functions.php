dbf6d9728d4bdf715cbd4adbbf2e54e130351623
cristian-ungureanu <cristian@themeisle.com>
2017-09-08 18:18:00 +0300

    apply filters on files that are added for hestia

--- obfx_modules/companion-legacy/inc/hestia/functions.php
+++ obfx_modules/companion-legacy/inc/hestia/functions.php
@@ -11,14 +11,14 @@
  */
 function themeisle_hestia_require() {
 	if ( function_exists( 'hestia_setup_theme' ) ) {
-		$sections_paths = array(
+		$sections_paths = apply_filters( 'themeisle_companion_hestia_sections',array(
 			'hestia/inc/features/import-zerif-content.php',
 			'hestia/inc/sections/hestia-features-section.php',
 			'hestia/inc/sections/hestia-testimonials-section.php',
 			'hestia/inc/sections/hestia-team-section.php',
 			'hestia/inc/sections/hestia-ribbon-section.php',
 			'hestia/inc/sections/hestia-clients-bar-section.php',
-		);
+		) );
 		themeisle_hestia_require_files( $sections_paths );
 	}
 }
@@ -29,14 +29,14 @@ function themeisle_hestia_require() {
  */
 function themeisle_hestia_load_controls() {
 	if ( function_exists( 'hestia_setup_theme' ) ) {
-		$features_paths = array(
+		$features_paths = apply_filters( 'themeisle_companion_hestia_controls',array(
 			'hestia/inc/features/feature-features-section.php',
 			'hestia/inc/features/feature-testimonials-section.php',
 			'hestia/inc/features/feature-team-section.php',
 			'hestia/inc/features/feature-ribbon-section.php',
 			'hestia/inc/features/feature-clients-bar-section.php',
 			'hestia/inc/customizer.php',
-		);
+		) );
 		themeisle_hestia_require_files( $features_paths );
 	}
 }
