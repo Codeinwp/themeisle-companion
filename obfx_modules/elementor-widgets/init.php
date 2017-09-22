b71997357026fa364bb668f65d5bfd9682d07118
Andrei Băicuș <baicusandrei@me.com>
2017-09-22 14:13:12 +0300

    Removed useless code.

--- obfx_modules/elementor-widgets/init.php
+++ obfx_modules/elementor-widgets/init.php
@@ -36,10 +36,6 @@ class Elementor_Widgets_OBFX_Module extends Orbit_Fox_Module_Abstract {
 		parent::__construct();
 		$this->name        = __( 'Elementor Modules', 'themeisle-companion' );
 		$this->description = __( 'Adds new Elementor Widgets.', 'themeisle-companion' );
-
-		if ( defined( 'ELEMENTOR_VERSION' ) ) {
-
-		}
 	}
 
 	/**
