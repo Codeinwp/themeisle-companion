8bacad7dba5d9331e6340927e2059c15e1f5340d
contactashish13 <contactashish13@gmail.com>
2017-10-25 12:54:33 +0530

    menu icon feature #82

--- obfx_modules/menu-icons/init.php
+++ obfx_modules/menu-icons/init.php
@@ -58,6 +58,34 @@ class Menu_Icons_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @access  public
 	 */
 	public function hooks() {
+		$this->loader->add_action( 'wp_update_nav_menu_item', $this, 'save_fields', 10, 3 );
+		$this->loader->add_filter( 'wp_edit_nav_menu_walker', $this, 'custom_walker', 99 );
+		$this->loader->add_filter( 'wp_setup_nav_menu_item', $this, 'show_menu', 10, 1 );
+
+	}
+
+	public function show_menu( $menu ) {
+		$icon	= get_post_meta( $menu->ID, 'obfx_menu_icon', true );
+		if ( ! empty( $icon ) ) {
+			$menu->icon = $icon;
+			if ( ! is_admin() ) {
+				// usually, icons are of the format fa-x or dashicons-x and when displayed they are displayed with classes 'fa fa-x' or 'dashicons dashicons-x'.
+				// so let's determine the prefix class.
+				$array			= explode( '-', $icon );
+				$prefix			= reset( $array );
+				$prefix			= apply_filters( 'obfx_menu_icons_icon_class', $prefix, $icon );
+				$menu->title	= sprintf( '<i class="obfx-menu-icon %s %s"></i>%s', $prefix, $icon, $menu->title );
+			}
+		}
+		return $menu;
+	}
+
+	public function custom_walker( $walker ) {
+		if ( ! class_exists( 'Menu_Icons_OBFX_Walker' ) ) {
+			require_once $this->get_dir() . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'class-nav-menu-edit-walker.php';
+		}
+		$walker = 'Menu_Icons_OBFX_Walker';
+		return $walker;
 	}
 
 	/**
@@ -69,7 +97,11 @@ class Menu_Icons_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @return array
 	 */
 	public function public_enqueue() {
-		return array();
+		return array(
+			'css' => array(
+				'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' => false,
+			),
+		);
 	}
 
 	/**
@@ -90,12 +122,23 @@ class Menu_Icons_OBFX_Module extends Orbit_Fox_Module_Abstract {
 			return array();
 		}
 
+		$this->localized	= array(
+			'admin'		=> array(
+				'icons'	=> apply_filters( 'obfx_menu_icons_icon_list', array() ),
+			),
+		);
+
 		return array(
 			'css' => array(
-				'admin' => false,
+				'vendor/bootstrap.min' => false,
+				'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' => array( 'vendor/bootstrap.min' ),
+				'vendor/fontawesome-iconpicker.min' => array( 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' ),
+				'admin' => array( 'vendor/fontawesome-iconpicker.min' ),
 			),
 			'js' => array(
-				'admin' => array( 'jquery' ),
+				'vendor/bootstrap.min' => array( 'jquery' ),
+				'vendor/fontawesome-iconpicker.min' => array( 'vendor/bootstrap.min' ),
+				'admin' => array( 'vendor/fontawesome-iconpicker.min' ),
 			),
 		);
 	}
@@ -109,5 +152,33 @@ class Menu_Icons_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 */
 	public function options() {
 		return array();
+	}
+
+
+	/**
+	 * Save menu item's icon.
+	 *
+	 * @access  public
+	 *
+	 * @param int   $menu_id         Nav menu ID.
+	 * @param int   $menu_item_db_id Menu item ID.
+	 * @param array $menu_item_args  Menu item data.
+	 */
+	public static function save_fields( $menu_id, $menu_item_db_id, $menu_item_args ) {
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			return;
+		}
+
+		$screen = get_current_screen();
+		if ( ! $screen instanceof WP_Screen || 'nav-menus' !== $screen->id ) {
+			return;
+		}
+
+		check_admin_referer( 'update-nav_menu', 'update-nav-menu-nonce' );
+
+		if ( isset( $_POST['menu-item-icon'][ $menu_item_db_id ] ) ) {
+			update_post_meta( $menu_item_db_id, 'obfx_menu_icon', $_POST['menu-item-icon'][ $menu_item_db_id ] );
+		}
+
 	}
 }
