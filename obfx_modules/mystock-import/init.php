9658dbec74fdd55084ab5bbb90452d56892206c2
contactashish13 <contactashish13@gmail.com>
2017-10-25 17:38:52 +0530

    initial

---
+++ obfx_modules/mystock-import/init.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * The module for mystock import.
+ *
+ * @link       https://themeisle.com
+ * @since      1.0.0
+ *
+ * @package    Mystock_Import_OBFX_Module
+ */
+
+/**
+ * The class for mystock import.
+ *
+ * @package    Mystock_Import_OBFX_Module
+ * @author     Themeisle <friends@themeisle.com>
+ * @codeCoverageIgnore
+ */
+class Mystock_Import_OBFX_Module extends Orbit_Fox_Module_Abstract {
+
+
+	/**
+	 * Mystock_Import_OBFX_Module constructor.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->name        = __( 'Mystock Import', 'themeisle-companion' );
+		$this->description = __( 'Module to import images from mystock.', 'themeisle-companion' );
+	}
+
+
+	/**
+	 * Determine if module should be loaded.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return bool	
+	 */
+	public function enable_module() {
+		return true;
+	}
+
+	/**
+	 * The loading logic for the module.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public function load() {
+	}
+
+	/**
+	 * Method to define hooks needed.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public function hooks() {
+
+	}
+
+	/**
+	 * Method that returns an array of scripts and styles to be loaded
+	 * for the front end part.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function public_enqueue() {
+		return array();
+	}
+
+	/**
+	 * Method that returns an array of scripts and styles to be loaded
+	 * for the admin part.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function admin_enqueue() {
+		$current_screen = get_current_screen();
+
+		if ( ! isset( $current_screen->id ) ) {
+			return array();
+		}
+		if ( $current_screen->id != 'nav-menus' ) {
+			return array();
+		}
+
+	}
+
+	/**
+	 * Method to define the options fields for the module
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function options() {
+		return array();
+	}
+}
