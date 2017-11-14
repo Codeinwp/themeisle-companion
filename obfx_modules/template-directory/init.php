768aeece925c5d71920d36dbba4799fe57368e34
Andrei Băicuș <baicusandrei@me.com>
2017-11-14 18:17:59 +0200

    Small fixes.

--- obfx_modules/template-directory/init.php
+++ obfx_modules/template-directory/init.php
@@ -10,6 +10,7 @@
  */
 
 use  Elementor\TemplateLibrary\Classes;
+
 /**
  * The class defines a new module to be used by Orbit Fox plugin.
  *
@@ -26,8 +27,9 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 */
 	public function __construct() {
 		parent::__construct();
-		$this->name        = __( 'Template Directory Module', 'themeisle-companion' );
-		$this->description = __( 'The awesome template directory is aiming to provide a wide range of templates that you can import straight into your website.', 'themeisle-companion' );
+		$this->name           = __( 'Template Directory Module', 'themeisle-companion' );
+		$this->description    = __( 'The awesome template directory is aiming to provide a wide range of templates that you can import straight into your website.', 'themeisle-companion' );
+		$this->active_default = true;
 	}
 
 	/**
@@ -48,6 +50,172 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @access  public
 	 */
 	public function load() {
+		return true;
+	}
+
+	/**
+	 * Method to define hooks needed.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 */
+	public function hooks() {
+		$this->loader->add_action( 'rest_api_init', $this, 'register_endpoints' );
+		//Add dashboard menu page.
+		$this->loader->add_action( 'admin_menu', $this, 'add_menu_page' );
+		//Add rewrite endpoint.
+		$this->loader->add_action( 'init', $this, 'demo_listing_register' );
+		//Add template redirect.
+		$this->loader->add_action( 'template_redirect', $this, 'demo_listing' );
+		//Remove customizer controls.
+		$this->loader->add_action( 'customize_register', $this, 'adjust_customizer', 1000 );
+		//Enqueue admin scripts.
+		$this->loader->add_action( 'admin_enqueue_scripts', $this, 'enqueue_template_dir_scripts' );
+		$this->loader->add_action( 'customize_controls_enqueue_scripts', $this, 'enqueue_template_dir_scripts' );
+	}
+
+	/**
+	 * Enqueue the scripts for the dashboard page of the
+	 */
+	public function enqueue_template_dir_scripts() {
+		$script_handle = $this->slug . '-script';
+		if ( ! defined( 'ELEMENTOR_VERSION' ) ) {
+			wp_enqueue_script( 'plugin-install' );
+			wp_enqueue_script( 'updates' );
+		}
+		wp_register_script( $script_handle, plugin_dir_url( $this->get_dir() ) . $this->slug . '/js/script.js', array( 'jquery' ), $this->version );
+		wp_localize_script( $script_handle, 'importer_endpoint',
+			array(
+				'url'   => $this->get_endpoint_url( '/import_elementor' ),
+				'nonce' => wp_create_nonce( 'wp_rest' ),
+			) );
+		wp_enqueue_script( $script_handle );
+	}
+
+	/**
+	 *
+	 *
+	 * @param string $path
+	 *
+	 * @return string
+	 */
+	public function get_endpoint_url( $path = '' ) {
+		return rest_url( $this->slug . $path );
+	}
+
+	/**
+	 * Method that returns an array of scripts and styles to be loaded
+	 * for the front end part.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array
+	 */
+	public function public_enqueue() {
+		return array();
+	}
+
+	/**
+	 * Method that returns an array of scripts and styles to be loaded
+	 * for the admin part.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @return array|boolean
+	 */
+	public function admin_enqueue() {
+		$current_screen = get_current_screen();
+
+		if ( ! isset( $current_screen->id ) ) {
+			return array();
+		}
+
+		if ( ! ( $current_screen->id == 'tools_page_obfx_template_dir' ) && ( ! $current_screen == 'customize' ) ) {
+			return array();
+		}
+
+		$enqueue = array(
+			'css' => array(
+				'admin' => array(),
+			),
+		);
+
+		if ( ! defined( 'ELEMENTOR_VERSION' ) ) {
+			$enqueue['js'] = array(
+				'plugin-install-helper' => array( 'jquery' ),
+			);
+		}
+
+		return $enqueue;
+	}
+
+	/**
+	 * Register Rest endpoint for requests.
+	 */
+	public function register_endpoints() {
+		register_rest_route( $this->slug, '/import_elementor', array(
+			'methods'  => 'POST',
+			'callback' => array( $this, 'import_elementor' ),
+		) );
+	}
+
+	/**
+	 * Remove the customizer controls and add the template listing control.
+	 */
+	public function adjust_customizer( $wp_customize ) {
+
+		if ( ! $this->is_template_dir_customize() ) {
+			return;
+		}
+		add_filter( 'customize_loaded_components', '__return_empty_array' );
+
+		//Remove all customizer sections and panels except 'obfx-templates'.
+		foreach ( $wp_customize->sections() as $section ) {
+			if ( $section->id !== 'obfx-templates' ) {
+				$wp_customize->remove_section( $section->id );
+			}
+		}
+		foreach ( $wp_customize->panels() as $panel ) {
+			if ( $panel->id !== 'widgets' && $panel->id !== 'nav_menus' ) {
+				$wp_customize->remove_panel( $panel->id );
+			}
+		}
+
+		//Get the module directory to later pass it for scripts enqueueing in the Orbit Fox customizer section.
+		$module_directory = $this->get_dir();
+
+		//Include the customizer section custom class and add the section.
+		require_once( $module_directory . '/inc/class-obfx-template-directory-customizer-section.php' );
+		if ( class_exists( 'OBFX_Template_Directory_Customizer_Section' ) ) {
+			$wp_customize->add_section(
+				new OBFX_Template_Directory_Customizer_Section(
+					$wp_customize, 'obfx-templates', array(
+						'priority'         => 0,
+						'module_directory' => plugin_dir_url( $this->get_dir() ),
+						'templates'        => $this->templates_list(),
+						'requires_plugins' => $this->render_view( 'template-plugin-install' ),
+					)
+				)
+			);
+		}
+
+	}
+
+	/**
+	 * Utility method to check if it's the customizer instance for the Template Directory Preview.
+	 *
+	 * @return bool
+	 */
+	public function is_template_dir_customize() {
+		//Check the URL parameter and bail if not on 'obfx_templates'.
+		$current = urldecode( isset( $_GET['url'] ) ? $_GET['url'] : '' );
+		$flag    = add_query_arg( 'obfx_templates', '', trailingslashit( home_url() ) );
+		$current = str_replace( '/', '', $current );
+		$flag    = str_replace( '/', '', $flag );
+		if ( $flag !== $current ) {
+			return false;
+		}
+
 		return true;
 	}
 
@@ -89,154 +257,6 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 		}
 
 		return $templates_list;
-	}
-
-	/**
-	 * Method to define hooks needed.
-	 *
-	 * @since   1.0.0
-	 * @access  public
-	 */
-	public function hooks() {
-		$this->loader->add_action( 'rest_api_init', $this, 'register_endpoints' );
-		//Add dashboard menu page.
-		$this->loader->add_action( 'admin_menu', $this, 'add_menu_page' );
-		//Add rewrite endpoint.
-		$this->loader->add_action( 'init', $this, 'demo_listing_register' );
-		//Add template redirect.
-		$this->loader->add_action( 'template_redirect', $this, 'demo_listing' );
-		//Remove customizer controls.
-		$this->loader->add_action( 'customize_register', $this, 'adjust_customizer' ,1000 );
-		//Enqueue admin scripts.
-		$this->loader->add_action( 'admin_enqueue_scripts', $this, 'enqueue_template_dir_scripts' );
-		$this->loader->add_action( 'customize_controls_enqueue_scripts', $this, 'enqueue_template_dir_scripts' );
-	}
-
-	/**
-	 * Enqueue the scripts for the dashboard page of the
-	 */
-	public function enqueue_template_dir_scripts() {
-		$script_handle = $this->slug . '-script';
-		if( ! defined( 'ELEMENTOR_VERSION' ) ) {
-			wp_enqueue_script( 'plugin-install' );
-			wp_enqueue_script( 'updates' );
-		}
-		wp_register_script( $script_handle, plugin_dir_url( $this->get_dir() ) . $this->slug . '/js/script.js', array( 'jquery' ), '1.0.0' );
-		wp_localize_script( $script_handle, 'importer_endpoint',
-			array(
-				'url'   => $this->get_endpoint_url( '/import_elementor' ),
-				'nonce' => wp_create_nonce( 'wp_rest' ),
-		) );
-		wp_enqueue_script( $script_handle );
-	}
-
-	/**
-	 * Method that returns an array of scripts and styles to be loaded
-	 * for the front end part.
-	 *
-	 * @since   1.0.0
-	 * @access  public
-	 * @return array
-	 */
-	public function public_enqueue() {
-		return array();
-	}
-
-	/**
-	 * Method that returns an array of scripts and styles to be loaded
-	 * for the admin part.
-	 *
-	 * @since   1.0.0
-	 * @access  public
-	 * @return array|boolean
-	 */
-	public function admin_enqueue() {
-		$current_screen = get_current_screen();
-
-		if ( ! isset( $current_screen->id ) ) {
-			return array();
-		}
-
-		if ( ! ( $current_screen->id == 'tools_page_obfx_template_dir' ) && ( ! $current_screen == 'customize' ) ) {
-			return array();
-		}
-
-		$enqueue = array(
-			'css' => array(
-				'admin' => array(),
-			),
-		);
-
-		if( ! defined( 'ELEMENTOR_VERSION' ) ) {
-			$enqueue['js'] = array(
-				'plugin-install-helper' => array( 'jquery' ),
-			);
-		}
-
-		return $enqueue;
-	}
-
-	/**
-	 *
-	 *
-	 * @param string $path
-	 *
-	 * @return string
-	 */
-	public function get_endpoint_url( $path = '' ) {
-		return rest_url( $this->slug . $path );
-	}
-
-	/**
-	 * Register Rest endpoint for requests.
-	 */
-	public function register_endpoints() {
-		register_rest_route( $this->slug, '/import_elementor', array(
-			'methods'  => 'POST',
-			'callback' => array( $this, 'import_elementor' ),
-		) );
-	}
-
-	/**
-	 * Remove the customizer controls and add the template listing control.
-	 */
-	public function adjust_customizer( $wp_customize ) {
-
-		if ( ! $this->is_template_dir_customize() ) {
-			return;
-		}
-		add_filter('customize_loaded_components', '__return_empty_array' );
-
-		//Remove all customizer sections and panels except 'obfx-templates'.
-		foreach ($wp_customize->sections() as $section){
-			if ( $section->id !== 'obfx-templates' ) {
-				$wp_customize->remove_section( $section->id );
-			}
-		}
-		foreach ($wp_customize->panels() as $panel){
-			if ( $panel->id!== 'widgets' && $panel->id !== 'nav_menus' ) {
-				$wp_customize->remove_panel( $panel->id );
-			}
-		}
-
-		//Get the module directory to later pass it for scripts enqueueing in the Orbit Fox customizer section.
-		$module_directory = $this->get_dir();
-
-		//Include the customizer section custom class and add the section.
-		require_once( $module_directory . '/inc/class-obfx-template-directory-customizer-section.php' );
-		if ( class_exists( 'OBFX_Template_Directory_Customizer_Section' ) ) {
-			$wp_customize->add_section(
-				new OBFX_Template_Directory_Customizer_Section(
-					$wp_customize, 'obfx-templates', array(
-						'priority'         => 0,
-						'module_directory' => plugin_dir_url( $this->get_dir() ),
-						'templates'        => $this->templates_list(),
-						'requires_plugins' => $this->render_view( 'template-plugin-install' ),
-					)
-				)
-			);
-		}
-
 	}
 
 	/**
@@ -282,8 +302,8 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 */
 	public function render_admin_page() {
 		$data = array(
-			'templates_array' => $this->templates_list(),
-			'requires_plugins'=> $this->render_view( 'template-plugin-install' )
+			'templates_array'  => $this->templates_list(),
+			'requires_plugins' => $this->render_view( 'template-plugin-install' )
 		);
 		echo $this->render_view( 'template-directory-page', $data );
 	}
@@ -292,14 +312,14 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * Utility method to call Elementor import routine.
 	 */
 	public function import_elementor() {
-		if( ! defined( 'ELEMENTOR_VERSION' ) ) {
+		if ( ! defined( 'ELEMENTOR_VERSION' ) ) {
 			return 'no-elementor';
 		}
 
-		require_once( ABSPATH . "wp-admin" . '/includes/file.php' );
-		require_once( ABSPATH . "wp-admin" . '/includes/image.php' );
+		require_once( ABSPATH . 'wp-admin' . '/includes/file.php' );
+		require_once( ABSPATH . 'wp-admin' . '/includes/image.php' );
 
-		$template = download_url( esc_url( $_POST['template_url'] ) );
+		$template                   = download_url( esc_url( $_POST['template_url'] ) );
 		$_FILES['file']['tmp_name'] = $template;
 		$elementor                  = new Elementor\TemplateLibrary\Source_Local;
 		$elementor->import_template();
@@ -355,7 +375,7 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 				break;
 			case 'zerif-lite':
 			case 'zerif-pro':
-			$new_template_page['page_template'] = 'template-fullwidth-no-title.php';
+				$new_template_page['page_template'] = 'template-fullwidth-no-title.php';
 				break;
 		}
 
@@ -372,49 +392,12 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	}
 
 	/**
-	 * Utility method to check if it's the customizer instance for the Template Directory Preview.
-	 *
-	 * @return bool
-	 */
-	public function is_template_dir_customize() {
-		//Check the URL parameter and bail if not on 'obfx_templates'.
-		$current = urldecode( isset( $_GET['url'] ) ? $_GET['url'] : '' );
-		$flag    = add_query_arg( 'obfx_templates', '', trailingslashit( home_url() ) );
-		$current = str_replace( '/', '', $current );
-		$flag    = str_replace( '/', '', $flag );
-		if ( $flag !== $current ) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
 	 * Options array for the Orbit Fox module.
 	 *
 	 * @return array
 	 */
 	public function options() {
 		return array();
-	}
-
-
-	/**
-	 * Check plugin state.
-	 *
-	 * @param string $slug plugin slug.
-	 *
-	 * @return bool
-	 */
-	public function check_plugin_state( $slug ) {
-		if ( file_exists( ABSPATH . 'wp-content/plugins/' . $slug . '/' . $slug . '.php' ) || file_exists( ABSPATH . 'wp-content/plugins/' . $slug . '/index.php' ) ) {
-			require_once( ABSPATH . "wp-admin" . '/includes/plugin.php' );
-			$needs = ( is_plugin_active( $slug . '/' . $slug . '.php' ) ||
-			           is_plugin_active( $slug . '/index.php' ) ) ?
-				'deactivate' : 'activate';
-			return $needs;
-		} else {
-			return 'install';
-		}
 	}
 
 	/**
@@ -445,7 +428,7 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 					break;
 				case 'activate':
 					$plugin_link_suffix = $slug . '/' . $slug . '.php';
-					$nonce = add_query_arg(
+					$nonce              = add_query_arg(
 						array(
 							'action'        => 'activate',
 							'plugin'        => rawurlencode( $plugin_link_suffix ),
@@ -454,11 +437,31 @@ class Template_Directory_OBFX_Module extends Orbit_Fox_Module_Abstract {
 							'_wpnonce'      => wp_create_nonce( 'activate-plugin_' . $plugin_link_suffix ),
 						), network_admin_url( 'plugins.php' )
 					);
-					$button .= '<a data-slug="' . $slug . '" class="activate-now button button-primary" href="' . esc_url( $nonce ) . '" aria-label="Activate ' . $slug . '">' . __( 'Activate', 'themeisle-companion' ) . '</a>';
+					$button             .= '<a data-slug="' . $slug . '" class="activate-now button button-primary" href="' . esc_url( $nonce ) . '" aria-label="Activate ' . $slug . '">' . __( 'Activate', 'themeisle-companion' ) . '</a>';
 					break;
 			}// End switch().
 		}// End if().
 		return $button;
+	}
+
+	/**
+	 * Check plugin state.
+	 *
+	 * @param string $slug plugin slug.
+	 *
+	 * @return bool
+	 */
+	public function check_plugin_state( $slug ) {
+		if ( file_exists( WP_CONTENT_DIR . '/plugins/' . $slug . '/' . $slug . '.php' ) || file_exists( WP_CONTENT_DIR . '/plugins/' . $slug . '/index.php' ) ) {
+			require_once( ABSPATH . 'wp-admin' . '/includes/plugin.php' );
+			$needs = ( is_plugin_active( $slug . '/' . $slug . '.php' ) ||
+			           is_plugin_active( $slug . '/index.php' ) ) ?
+				'deactivate' : 'activate';
+
+			return $needs;
+		} else {
+			return 'install';
+		}
 	}
 
 }
