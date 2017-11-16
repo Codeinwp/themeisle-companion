9f5641fd0654e18140d7fcde517432b7e2c1c08b
selu91 <cristea.marius.alexandru@gmail.com>
2017-11-16 14:13:42 +0200

    Release. Adds compatibility with WordPress 4.9.

--- themeisle-companion.php
+++ themeisle-companion.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Orbit Fox Companion
  * Plugin URI:        https://themeisle.com/plugins/orbit-fox-companion
  * Description:       Enhances ThemeIsle's themes with extra functionality.
- * Version:           2.1.0
+ * Version:           2.1.1
  * Author:            Themeisle
  * Author URI:        https://themeisle.com
  * License:           GPL-2.0+
