df066d002cbdeda190782ad4b29ed5313137b838
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-11-27 16:29:53 +0200

    Update version

--- themeisle-companion.php
+++ themeisle-companion.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Orbit Fox Companion
  * Plugin URI:        https://themeisle.com/plugins/orbit-fox-companion
  * Description:       Enhances ThemeIsle's themes with extra functionality.
- * Version:           2.2.2
+ * Version:           2.2.3
  * Author:            Themeisle
  * Author URI:        https://themeisle.com
  * License:           GPL-2.0+
