8d99d5c529d4c678bcdb657f09e936dab8d7d47f
Andrei Baicus <baicusandrei@me.com>
2018-02-26 18:34:51 +0200

    [WIP] Analytics Module.

--- obfx_modules/google-analytics/init.php
+++ obfx_modules/google-analytics/init.php
@@ -15,6 +15,10 @@
  * @author     Themeisle <friends@themeisle.com>
  */
 class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
+	/**
+	 * @var string Uptime api endpoint.
+	 */
+	private $api_url = 'https://analytics.orbitfox.com/api/pirate-bridge/v1';
 
 	/**
 	 * Test_OBFX_Module constructor.
@@ -56,27 +60,6 @@ class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @access  public
 	 */
 	public function after_options_save() {
-		$this->activate();
-	}
-
-	/**
-	 * Method invoked after options save.
-	 *
-	 * @since   2.3.3
-	 * @access  public
-	 */
-	public function activate() {
-		$email = sanitize_email( $this->get_option( 'monitor_email' ) );
-		if ( ! is_email( $email ) ) {
-			return;
-		}
-
-		$monitor_url = $this->monitor_url . '/api/monitor/create';
-		$url         = home_url();
-		$args        = array(
-			'body' => array( 'url' => $url, 'email' => $email )
-		);
-		$response    = wp_remote_post( $monitor_url, $args );
 	}
 
 	/**
@@ -85,26 +68,8 @@ class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @since   2.3.3
 	 * @access  public
 	 */
-	public function before_options_save( $options ) {
+	public function before_options_save() {
 		$this->deactivate();
-	}
-
-	/**
-	 * Method called on module deactivation.
-	 * Calls the API to unregister an url from the monitor.
-	 *
-	 * @since   2.3.3
-	 * @access  public
-	 */
-	public function deactivate() {
-
-		$monitor_url  = $this->monitor_url . '/api/monitor/remove';
-		$url          = home_url();
-		$args         = array(
-			'body' => array( 'url' => $url )
-		);
-		$response     = wp_remote_post( $monitor_url, $args );
-		$api_response = json_decode( $response['body'] );
 	}
 
 	/**
@@ -114,8 +79,62 @@ class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @access  public
 	 */
 	public function hooks() {
-		$this->loader->add_action( $this->get_slug() . '_before_options_save', $this, 'before_options_save', 10, 1 );
-		$this->loader->add_action( $this->get_slug() . '_after_options_save', $this, 'after_options_save' );
+		$this->loader->add_action( 'rest_api_init', $this, 'register_endpoints' );
+		$this->loader->add_action( 'current_screen', $this, 'maybe_save_obfx_token' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $this, 'enqueue_analytics_scripts' );
+		$this->loader->add_filter( 'obfx_custom_control_google_signin', $this, 'generate_analytics_login' );
+		$this->loader->add_filter( 'obfx_custom_control_analytics_accounts_refresh', $this, 'render_refresh_control' );
+		$this->loader->add_filter( 'obfx_custom_control_analytics_accounts_unregister', $this, 'render_unregister_control' );
+		$this->loader->add_action( 'wp_head', $this, 'output_analytics_code', 0 );
+	}
+
+	/**
+	 * Register endpoint for refreshing analytics.
+	 */
+	public function register_endpoints() {
+		register_rest_route( 'obfx-' . $this->slug, '/obfx-analytics', array(
+			array(
+				'methods'  => WP_REST_Server::CREATABLE,
+				'callback' => array( $this, 'refresh_tracking_links' )
+			),
+		) );
+	}
+
+	public function refresh_tracking_links() {
+		if ( ! is_admin() ) {
+			return false;
+		}
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return false;
+		}
+
+		$obfx_token = get_option( 'obfx_token', '' );
+
+		if ( ( $_POST['deactivate'] === 'unregister' ) ) {
+			return $this->unregister_website( $obfx_token );
+		}
+		if ( empty( $obfx_token ) ) {
+			return new WP_Error( '200', 'Your site is not registered.' );
+		}
+		$this->get_tracking_codes( $obfx_token, true );
+	}
+
+	public function unregister_website( $obfx_token ) {
+		if ( ! isset( $obfx_token ) ) {
+			return false;
+		}
+		delete_option( 'obfx_token' );
+		delete_option( 'obfx_google_accounts_tracking_codes' );
+		$req_headers = array( 'x-obfx-auth' => $obfx_token );
+		$req_body    = array( 'site_url' => home_url(), 'site_hash' => $this->get_site_hash() );
+
+		$request = wp_remote_post( $this->api_url . '/remove_website',
+			array(
+				'headers' => $req_headers,
+				'body'    => $req_body,
+			) );
+
+		return $request;
 	}
 
 	/**
@@ -130,6 +149,28 @@ class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
 		return array();
 	}
 
+	public function enqueue_analytics_scripts() {
+		$script_handle = $this->slug . '-script';
+		wp_register_script( $script_handle, plugin_dir_url( $this->get_dir() ) . $this->slug . '/js/script.js', array( 'jquery' ), $this->version );
+		wp_localize_script( $script_handle, 'obfxAnalyticsObj',
+			array(
+				'url'   => $this->get_endpoint_url( '/obfx-analytics' ),
+				'nonce' => wp_create_nonce( 'wp_rest' ),
+			) );
+		wp_enqueue_script( $script_handle );
+	}
+
+	/**
+	 * Returns rest endpoint url.
+	 *
+	 * @param string $path
+	 *
+	 * @return string
+	 */
+	public function get_endpoint_url( $path = '' ) {
+		return rest_url( 'obfx-' . $this->slug . $path );
+	}
+
 	/**
 	 * Method that returns an array of scripts and styles to be loaded
 	 * for the admin part.
@@ -139,23 +180,7 @@ class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @return array|boolean
 	 */
 	public function admin_enqueue() {
-		$current_screen = get_current_screen();
-
-		if ( ! isset( $current_screen->id ) ) {
-			return array();
-		}
-		if ( $current_screen->id != 'dashboard' ) {
-			return array();
-		}
-
-		return array(
-			'js'  => array(
-				'stats' => array( 'jquery' ),
-			),
-			'css' => array(
-				'stats' => false,
-			),
-		);
+		return array();
 	}
 
 	/**
@@ -166,41 +191,179 @@ class Google_Analytics_OBFX_Module extends Orbit_Fox_Module_Abstract {
 	 * @return array
 	 */
 	public function options() {
+		$token = get_option( 'obfx_token', '' );
+		if ( empty( $token ) ) {
+			return array(
+				array(
+					'id'   => 'google_signin',
+					'name' => 'google_signin',
+					'type' => 'custom',
+				),
+			);
+		}
+
+		$options = array( '-' => __( 'Select a tracking code', 'themeisle-companion' ) . '...' );
+
+		$accounts = get_option( 'obfx_google_accounts_tracking_codes', array() );
+
+		if ( ! empty ( $accounts ) ) {
+			foreach ( $accounts as $account ) {
+				$options[ $account->tracking_code ] = $account->account_name . ' - ' . $account->tracking_code;
+			}
+		}
+
 		return array(
+
 			array(
-				'id'          => 'google_signin',
-				'name'        => 'google_signin',
-				'title'       => '',
-				'description' => 'Sign in to use the Google Analytics integration.',
-				'type'        => 'custom',
-				'default'     => '',
+				'id'   => 'analytics_accounts_refresh',
+				'name' => 'analytics_accounts_refresh',
+				'type' => 'custom',
+			),
+			array(
+				'id'      => 'analytics_accounts_select',
+				'name'    => 'analytics_accounts_select',
+				'type'    => 'select',
+				'options' => $options,
+				'default' => '-',
+			),
+			array(
+				'id'   => 'analytics_accounts_unregister',
+				'name' => 'analytics_accounts_unregister',
+				'type' => 'custom',
 			)
 		);
 	}
 
 	/**
-	 * Renders a custom control.
-	 * @param array $option
+	 * Render the refresh button.
 	 *
 	 * @return string
 	 */
-	public function render_custom_control( $option = array() ) {
-		return $this->generate_analytics_login();
+	public function render_refresh_control() {
+		$refresh_button = '<button id="refresh-analytics-accounts" class="btn btn-primary btn-sm"><i class="dashicons dashicons-update"></i> ' . __( 'Refresh Accounts', 'themeisle-companion' ) . '</button><p></p>';
+
+		return $refresh_button;
 	}
 
-	private final function generate_analytics_login() {
-		$url      = 'http://ea435eb1.ngrok.io/api/pirate-bridge/v1/auth?';
-		$url      .= http_build_query( array(
-				'site_hash' => $this->get_site_hash(),
-				'site_url' => home_url(),
-				'site_return' => admin_url( 'admin.php?page=obfx_companion' ),
-			)
+	/**
+	 * Render the unregister button.
+	 *
+	 * @return string
+	 */
+	public function render_unregister_control() {
+		$unregister_button = '<button id="unregister-analytics" class="btn btn-sm"><i class="dashicons dashicons-no"></i>' . __( 'Unregister Site', 'themeisle-companion' ) . '</button><p>';
+
+		return $unregister_button;
+	}
+
+	/**
+	 * Get tracking codes from server.
+	 *
+	 * @param string $obfx_token
+	 * @param bool $forced
+	 *
+	 * @return bool|string
+	 */
+	public function get_tracking_codes( $obfx_token = '', $forced = false ) {
+		if ( ! isset( $obfx_token ) ) {
+			return false;
+		}
+
+		$req_headers = array( 'x-obfx-auth' => $obfx_token );
+		$req_body    = array(
+			'site_url'      => home_url(),
+			'site_hash'     => $this->get_site_hash(),
+			'forced_update' => 'not_forced'
 		);
-		$template = '<a class="btn btn-lg" href="' . esc_url( $url ) . '">' . __( 'Login with Google', 'themeisle-companion' ) .'</a>';
+
+		if ( $forced === true ) {
+			$req_body['forced_update'] = 'forced_update';
+		}
+
+		$request = wp_remote_post( $this->api_url . '/get_tracking_links',
+			array(
+				'headers' => $req_headers,
+				'body'    => $req_body,
+			) );
+		if ( empty ( $request['body'] ) ) {
+			return false;
+		}
+		$accounts = json_decode( $request['body'] );
+		if ( empty( $accounts ) ) {
+			return false;
+		}
+
+		update_option( 'obfx_google_accounts_tracking_codes', $accounts );
+	}
+
+	/**
+	 * Generates the analytics login control.
+	 *
+	 * @return string
+	 */
+	public function generate_analytics_login() {
+		$url = $this->api_url . '/auth';
+		$url = add_query_arg( array(
+			'site_hash'   => $this->get_site_hash(),
+			'site_url'    => home_url(),
+			'site_return' => admin_url( 'admin.php?page=obfx_companion' ),
+		), $url );
+
+		$template = '<a class="btn btn-success" href="' . esc_url( $url ) . '"><span class="dashicons dashicons-googleplus obfx-google"></span>' . __( 'Authenticate with Google', 'themeisle-companion' ) . '</a>';
+
 		return $template;
 	}
 
+	/**
+	 * Generate a website hash.
+	 *
+	 * @return string
+	 */
 	private final function get_site_hash() {
-		return  mb_strimwidth( rtrim( ltrim( sanitize_text_field( preg_replace('/[^a-zA-Z0-9]/', '', AUTH_KEY . SECURE_AUTH_KEY . LOGGED_IN_KEY )  ) ) ), 0, 100 );
+		return mb_strimwidth( rtrim( ltrim( sanitize_text_field( preg_replace( '/[^a-zA-Z0-9]/', '', AUTH_KEY . SECURE_AUTH_KEY . LOGGED_IN_KEY ) ) ) ), 0, 100 );
+	}
+
+	public final function maybe_save_obfx_token() {
+		$obfx_token = isset( $_GET['obfx_token'] ) ? sanitize_text_field( $_GET['obfx_token'] ) : '';
+		if ( empty( $obfx_token ) ) {
+			return '';
+		}
+		if ( ! is_admin() ) {
+			return '';
+		}
+		$current_screen = get_current_screen();
+		if ( ! isset( $current_screen->id ) ) {
+			return '';
+		}
+		if ( $current_screen->id !== 'toplevel_page_obfx_companion' ) {
+			return '';
+		}
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return '';
+		}
+		update_option( 'obfx_token', $obfx_token );
+		$this->get_tracking_codes( $obfx_token );
+		wp_safe_redirect( admin_url( 'admin.php?page=obfx_companion' ) );
+
+	}
+
+	public final function output_analytics_code() {
+		$ua_code = $this->get_option( 'analytics_accounts_select' ); ?>
+        <!-- Google Analytics -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr( $ua_code ); ?>"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push( arguments );
+            }
+
+            gtag( 'js', new Date() );
+
+            gtag( 'config', '<?php echo esc_attr( $ua_code ); ?>' );
+        </script>
+        <!-- End Google Analytics -->
+		<?php
 	}
 }
