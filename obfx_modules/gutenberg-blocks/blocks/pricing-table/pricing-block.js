45fc1ea23525b60a17829c88963108efb76bd9f6
Hardeep Asrani <hardeepasrani@gmail.com>
2018-08-09 02:10:09 +0000

    Revamped Pricing Table Block
    Improved Pricing Table UI, and took it from its old form to a Parent/Child format to make it more extensible.

---
+++ obfx_modules/gutenberg-blocks/blocks/pricing-table/pricing-block.js
@@ -0,0 +1,165 @@
+/**
+ * WordPress dependencies...
+ */
+
+import classnames from 'classnames';
+
+const { __ } = wp.i18n;
+
+const {
+	registerBlockType
+} = wp.blocks;
+
+const {
+	InnerBlocks,
+	BlockControls,
+	InspectorControls,
+	PanelColorSettings,
+} = wp.editor;
+
+const { 
+	Dashicon,
+	Toolbar,
+	Button,
+	Tooltip,
+} = wp.components;
+
+/**
+ * Internal dependencies
+ */
+registerBlockType( 'orbitfox/pricing-block', {
+	title: __( 'Pricing Block' ),
+	parent: [ 'orbitfox/pricing-table' ],
+	icon: 'slides',
+	category: 'layout',
+	keywords: [
+		'pricing',
+		'table',
+		'orbitfox'
+	],
+	attributes: {
+		featured: {
+			type: 'boolean',
+			default: false,
+		},
+		backgroundColor: {
+			type: 'string',
+			default: '#ffffff',
+		},
+	},
+
+	edit( props ) {
+		const toggleFeatured = () => {
+			props.setAttributes( { featured: !props.attributes.featured } );
+		};
+		const setBackgroundColor = value => {
+			props.setAttributes( { backgroundColor: value } );
+		}
+		const TEMPLATE =  [
+			['core/heading', {
+				content: __( 'Basic' ),
+				align: 'center',
+				level: 5,
+			}],
+			['core/paragraph', {
+				content: __( '$9.99' ),
+				align: 'center',
+				customFontSize: 36,
+			}],
+			['core/paragraph', {
+				content: __( 'Per Month' ),
+				align: 'center',
+				customFontSize: 12,
+			}],
+			['core/separator', {}],
+			['core/paragraph', {
+				content: __( 'First Feature' ),
+				align: 'center',
+				fontSize: 'small',
+			}],
+			['core/separator', {}],
+			['core/paragraph', {
+				content: __( 'Second Feature' ),
+				align: 'center',
+				fontSize: 'small',
+			}],
+			['core/separator', {}],
+			['core/paragraph', {
+				content: __( 'Last Feature' ),
+				align: 'center',
+				fontSize: 'small',
+			}],
+			['core/separator', {}],
+			['core/button', {
+				text: __('Buy Now'),
+				align: 'center',
+			}],
+		];
+
+		return [
+			<BlockControls key="toolbar-controls">
+				<Toolbar
+					className='components-toolbar'
+				>
+					<Tooltip text={ __( 'Feature Table' )	}>
+						<Button
+							className={ classnames(
+								'components-icon-button',
+								'components-toolbar__control',
+								{ 'is-active': props.attributes.featured },
+							) }
+							onClick={ toggleFeatured }
+						>
+							<Dashicon icon="star-empty" />
+						</Button>
+					</Tooltip>
+				</Toolbar>
+			</BlockControls>,
+
+			<InspectorControls>
+				<PanelColorSettings
+					title={ __( 'Color Settings' ) }
+					initialOpen={ true }
+					colorSettings={ [
+						{
+							value: props.attributes.backgroundColor,
+							onChange: setBackgroundColor,
+							label: __( 'Background Color' ),
+						},
+					] }
+				>
+				</PanelColorSettings>
+			</InspectorControls>,
+
+			<div
+				className={ classnames(
+					'wp-block-column',
+					{ 'raised': props.attributes.featured },
+				) }
+				style={ {
+					backgroundColor: props.attributes.backgroundColor,
+				}}
+			>
+				<InnerBlocks
+					template={ TEMPLATE }
+				/>
+			</div>
+		];
+	},
+
+	save( props ) {
+		return (
+			<div
+				className={ classnames(
+					'wp-block-column',
+					{ 'raised': props.attributes.featured },
+				) }
+				style={ {
+					backgroundColor: props.attributes.backgroundColor,
+				} }
+			>
+				<InnerBlocks.Content/>
+			</div>
+		);
+	},
+});
---
+++ obfx_modules/gutenberg-blocks/blocks/pricing-table/pricing-table.js
@@ -0,0 +1,41 @@
+/**
+ * WordPress dependencies...
+ */
+
+const { __ } = wp.i18n;
+
+const { registerBlockType } = wp.blocks;
+
+const { InnerBlocks } = wp.editor;
+
+registerBlockType( 'orbitfox/pricing-table', {
+	title: __( 'Pricing Table' ),
+	icon: 'slides',
+	category: 'layout',
+	keywords: [
+		'pricing',
+		'table',
+		'orbitfox'
+	],
+
+	edit( { className } ) {
+		const ALLOWED_BLOCKS = [ 'orbitfox/pricing-block' ];
+		const TEMPLATE = [ [ 'orbitfox/pricing-block' ], [ 'orbitfox/pricing-block' ], [ 'orbitfox/pricing-block' ] ];
+		return (
+			<div className={ className }>
+				<InnerBlocks
+					allowedBlocks={ ALLOWED_BLOCKS }
+					template={ TEMPLATE }
+				/>
+			</div>
+		);
+	},
+
+	save() {
+		return (
+			<div className="wp-block-orbitfox-pricing-table">
+				<InnerBlocks.Content/>
+			</div>
+		);
+	},
+});
