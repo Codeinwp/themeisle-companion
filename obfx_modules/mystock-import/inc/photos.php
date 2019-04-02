aa54ee04beb77aa9642f846606ab1de76578d410
selul <marius.cristea@vertistudio.com>
2019-04-02 13:40:01 +0300

    fix importing on next page for infinite scroll, fix #341

--- obfx_modules/mystock-import/inc/photos.php
+++ obfx_modules/mystock-import/inc/photos.php
@@ -11,25 +11,7 @@
 			<?php
 			if ( ! empty( $urls ) ) {
 				foreach ( $urls as $photo ) {
-					$pid = $photo['id'];
-					if ( ! empty( $pid ) ) {
-						$thumb = $photo['url_m'];
-						?>
-						<li class='obfx-image' data-page="1" data-pid="<?php echo esc_attr( $pid ); ?>" data-url="<?php echo esc_attr( $photo['url_l'] ); ?>">
-							<div class="obfx-preview">
-								<div class="thumbnail">
-									<div class="centered">
-										<img src='<?php echo esc_url( $thumb ); ?>'>
-									</div>
-								</div>
-							</div>
-							<button type="button" class="check obfx-image-check" tabindex="0">
-								<span class="media-modal-icon"></span>
-								<span class="screen-reader-text"><?php esc_html_e( 'Deselect', 'themeisle-companion' ); ?></span>
-							</button>
-						</li>
-						<?php
-					}
+					include ( dirname( __FILE__ ) ) . '/photo.php';
 				}
 			}
 			?>
