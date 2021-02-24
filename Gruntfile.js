79e89698ac870de42561cdf67eda618e418dfe87
cristian-ungureanu <cristian@themeisle.com>
2021-02-24 14:52:45 +0200

    fix: add grunt task to replace the textdomain and another one to generate the pot file [#622]

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,10 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-version');
 	grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+  grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
 	grunt.initConfig({
+		package: grunt.file.readJSON('package.json'),
 		version: {
 			json: {
 				options: {
@@ -25,7 +28,42 @@ module.exports = function (grunt) {
 					flags: ''
 				},
 				src: [ 'core/includes/class-orbit-fox.php' ]
-
+			},
+			addtextdomain: {
+				options: {
+					textdomain: '<%= package.textdomain %>',
+					updateDomains: ['textdomain']
+				},
+				target: {
+					files: {
+						src: [
+							'vendor/codeinwp/**/*.php'
+						]
+					}
+				}
+			}
+		},
+		makepot: {
+			target: {
+				options: {
+					cwd: '',
+					domainPath: 'languages',
+					exclude: ['node_modules/.*'],
+					include: ['.*', 'vendor/codeinwp/.*'],
+					mainFile: 'themeisle-companion.php',
+					potFilename: 'themeisle-companion.pot',
+					potHeaders: {
+						poedit: true,
+						'x-poedit-keywordslist': true,
+						'report-msgid-bugs-to': '<%= package.pot.reportmsgidbugsto %>',
+						'language-team': '<%= package.pot.languageteam %>',
+						'last-translator': '<%= package.pot.lasttranslator %>',
+					},
+					processPot: null,
+					type: 'wp-plugin',
+					updateTimestamp: true,
+					updatePoFiles: false
+				}
 			}
 		},
 		wp_readme_to_markdown: {
