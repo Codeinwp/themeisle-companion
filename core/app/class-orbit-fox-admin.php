81f16e9c28326e7ab0566ab6001d4086e084431b
Marius Cristea <marius.cristea@vertistudio.com>
2022-02-16 09:07:06 +0000

    Update core/app/class-orbit-fox-admin.php

--- core/app/class-orbit-fox-admin.php
+++ core/app/class-orbit-fox-admin.php
@@ -423,7 +423,7 @@ class Orbit_Fox_Admin {
 				'banner'      => esc_url( OBFX_URL ) . '/dashboard/assets/wp-landing.jpg',
 				'name'        => 'WP Landing Kit',
 				'description' => __( 'Turn WordPress into a landing page powerhouse with Landing Kit. Map domains to pages or any other published resource.', 'themeisle-companion' ),
-				'author'      => 'ThemeIsle',
+				'author'      => 'Themeisle',
 				'action'      => 'external',
 				'url'         => 'https://wplandingkit.com/?utm_medium=orbitfoxdashboard&utm_source=recommendedplugins&utm_campaign=orbitfox',
 				'premium'     => true,
