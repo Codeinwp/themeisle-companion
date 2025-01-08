2d323f896cc25ae4f821c415156e0fa679ed49ba
girishpanchal30 <girish@krishaweb.com>
2025-01-08 16:01:27 +0530

    fix: reviewer feedback

--- obfx_modules/elementor-extra-widgets/widgets/elementor/pricing-table.php
+++ obfx_modules/elementor-extra-widgets/widgets/elementor/pricing-table.php
@@ -1039,7 +1039,7 @@ class Pricing_Table extends Widget_Base {
 			$output .= '<div class="obfx-title-wrapper">';
 			if ( ! empty( $settings['title'] ) ) {
 				// Start of title tag.
-				$title_tag = in_array( $settings['title_tag'], array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p' ), true ) ? $settings['title_tag'] : 'h1';
+				$title_tag = $this->sanitize_tag( $settings['title_tag'] );
 				$output   .= '<' . esc_html( $title_tag ) . ' ' . $this->get_render_attribute_string( 'title' ) . '>';
 
 				// Title string.
@@ -1050,7 +1050,7 @@ class Pricing_Table extends Widget_Base {
 			}
 			if ( ! empty( $settings['subtitle'] ) ) {
 				// Start of subtitle tag.
-				$subtitle_tag = in_array( $settings['subtitle_tag'], array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p' ), true ) ? $settings['subtitle_tag'] : 'h1';
+				$subtitle_tag = $this->sanitize_tag( $settings['subtitle_tag'] );
 				$output      .= '<' . esc_html( $subtitle_tag ) . ' ' . $this->get_render_attribute_string( 'subtitle' ) . '>';
 
 				// Subtitle string.
@@ -1158,6 +1158,17 @@ class Pricing_Table extends Widget_Base {
 			$output .= '</span>';
 		}
 		return $output;
+	}
+
+	/**
+	 * Sanitize html tags.
+	 *
+	 * @param string $tag HTML tagname.
+	 *
+	 * @return string
+	 */
+	private function sanitize_tag( $tag ) {
+		return in_array( $tag, array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p' ), true ) ? $tag : 'h1';
 	}
 }
 
