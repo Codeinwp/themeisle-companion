98065324d206608d8ed1ba63b29f2a11463c067d
abaicus <baicusandrei@me.com>
2025-08-07 17:37:51 +0300

    chore: rename login customizer button section [closes #914]

--- obfx_modules/login-customizer/js/src/common/constants.js
+++ obfx_modules/login-customizer/js/src/common/constants.js
@@ -4,7 +4,7 @@ const SECTIONS = {
   logo: __("Logo", "themeisle-companion"),
   background: __("Background", "themeisle-companion"),
   formStyling: __("Form", "themeisle-companion"),
-  loginButton: __("Login Button", "themeisle-companion"),
+  loginButton: __("Buttons", "themeisle-companion"),
   formFooter: __("Form Footer", "themeisle-companion"),
   urlSettings: __("URL Settings", "themeisle-companion"),
   additionalOptions: __("Additional Options", "themeisle-companion"),
