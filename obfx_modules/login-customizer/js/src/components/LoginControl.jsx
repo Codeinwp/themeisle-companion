eecb2c7854bd79d38bde24767f065cd88af74202
abaicus <baicusandrei@me.com>
2025-08-07 12:25:27 +0300

    fix: add back loader when customizer section is not focused

--- obfx_modules/login-customizer/js/src/components/LoginControl.jsx
+++ obfx_modules/login-customizer/js/src/components/LoginControl.jsx
@@ -1,5 +1,4 @@
-import { Box, Text } from '@chakra-ui/react';
-import { __ } from '@wordpress/i18n';
+import { Box, Flex, Spinner } from '@chakra-ui/react';
 import { OptionsProvider } from '../hooks/useLoginCustomizerOptions';
 import { useSectionLoaded } from '../hooks/useSectionLoaded';
 import Footer from './Footer';
@@ -11,22 +10,16 @@ const LoginControl = () => {
   const { sectionLoaded } = useSectionLoaded(sectionID);
 
   if (!sectionLoaded) {
-    // return (
-    //   <Flex justifyContent="center" alignItems="center" height="100%">
-    //     <Spinner />
-    //   </Flex>
-    // ); 
+    return (
+      <Flex justifyContent="center" alignItems="center" height="100%">
+        <Spinner />
+      </Flex>
+    ); 
   }
 
   return (
     <OptionsProvider>    
-      {/* <Box p={4} borderBottom="1px solid" borderColor="gray.200" bg="gray.50">
-        <Text fontSize="xs" color="gray.700" m={0}>{
-          __('The Login Page Customizer allows you to customize the appearance of your WordPress login page. You can change colors, upload a custom logo, modify form styling, and configure security settings.', 'themeisle-companion')}</Text>
-      </Box> */}
-
       <Sections/>
-
 
       <Box mt="auto" mb="-12px">
         <Footer />
