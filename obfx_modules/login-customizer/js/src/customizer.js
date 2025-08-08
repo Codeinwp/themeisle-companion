385039aaf1131de4a2f7c524397aace95bd695e8
abaicus <baicusandrei@me.com>
2025-08-08 13:22:29 +0300

    chore: adds polyfills for older WP core versions

--- obfx_modules/login-customizer/js/src/customizer.js
+++ obfx_modules/login-customizer/js/src/customizer.js
@@ -1,9 +1,12 @@
 import { render } from "@wordpress/element";
 import LoginControl from "./components/LoginControl";
 import { Provider } from "./components/ui/provider";
+import maybeAddPolyFills from "../../../../dashboard/src/wp-element-polyfill";
 
 const { customize } = window.wp;
 const { controlConstructor, Control } = customize;
+
+maybeAddPolyFills();
 
 controlConstructor.obfx_login_control = Control.extend({
   renderContent: function renderContent() {
